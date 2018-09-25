b139e8a75bf3feba69124ff30e669839b38c2e43
Juan Rodríguez <juan.rodriguez@pccomponentes.com>
2018-09-25 08:14:56 +0200

    Minor fixes

--- tests/Util/OutputTest.php
+++ tests/Util/OutputTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * This disaster was designed by
+ * @author Juan G. Rodríguez Carrión <juan.rodriguez@pccomponentes.com>
+ */
 declare(strict_types=1);
 namespace Pccomponentes\Ganchudo\Tests\Util;
 
