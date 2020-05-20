b2184535d75b6ddca41c679f08b05f7bcc7006d6
Bryan <sersoker@gmail.com>
2020-05-20 14:20:41 +0200

    Fix in TreeBuilder construct call

--- src/Config/Configuration.php
+++ src/Config/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ganchudo');
+        $treeBuilder = new TreeBuilder('ganchudo');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
